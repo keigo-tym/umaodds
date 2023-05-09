53ce8dbd1eccee8717d11d87a8227903246bfc71
keigo-toyama <104549980+keigo-tym@users.noreply.github.com>
2023-05-09 15:24:06 +0900

    horsesテーブルのマイグレーションファイル

--- database/migrations/2023_05_09_055621_create_horses_table.php
+++ database/migrations/2023_05_09_055621_create_horses_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('horses', function (Blueprint $table) {
             $table->id();
+            // レースID
+            $table->integer('race_id');
+            // 枠番
+            $table->integer('frame_number');
+            // 馬番
+            $table->integer('horse_number');
+            // 馬名
+            $table->string('name');
+            // 前売りオッズ
+            $table->decimal('advance_odds', 5, 1)->nullable();
+            // 前日オッズ
+            $table->decimal('previous_odds', 5, 1)->nullable();
+            // 当日12時オッズ
+            $table->decimal('twelve_odds', 5, 1)->nullable();
+            // 当日15時オッズ
+            $table->decimal('fifteen_odds', 5, 1)->nullable();
             $table->timestamps();
         });
     }
