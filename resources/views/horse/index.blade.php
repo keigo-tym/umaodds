c664178fc60e62280caf0e9b859d4edb03000ce8
keigo-toyama <104549980+keigo-tym@users.noreply.github.com>
2023-05-09 16:01:38 +0900

    horse.indexのビュー

---
+++ resources/views/horse/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.umaodds')
+
+@section('title', 'Horse.index')
+    
+@section('menubar')
+    @parent
+    出走馬ページ
+@endsection
+
+@section('content')
+    <table>
+    <tr><th>race_id</th><th>frame_number</th><th>horse_number</th><th>name</th>advance_odds</tr>
+    @foreach ($items as $item)
+        <tr>
+            <td>{{$item->race_id}}</td>
+            <td>{{$item->frame_number}}</td>
+            <td>{{$item->horse_number}}</td>
+            <td>{{$item->name}}</td>
+            <td>{{$item->advance_odds}}</td>
+        </tr>
+    @endforeach
+    </table>
+@endsection
+
+@section('footer')
+copyright 2023 kei
+@endsection
