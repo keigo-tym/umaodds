0cbc544543ac3f2166960bc30371e1bdcb0e14bc
keigo-toyama <104549980+keigo-tym@users.noreply.github.com>
2023-05-09 16:18:08 +0900

    ビューの修正

--- resources/views/horse/index.blade.php
+++ resources/views/horse/index.blade.php
@@ -9,7 +9,7 @@
 
 @section('content')
     <table>
-    <tr><th>race_id</th><th>frame_number</th><th>horse_number</th><th>name</th>advance_odds</tr>
+    <tr><th>race_id</th><th>frame_number</th><th>horse_number</th><th>name</th><th>advance_odds</th></tr>
     @foreach ($items as $item)
         <tr>
             <td>{{$item->race_id}}</td>
