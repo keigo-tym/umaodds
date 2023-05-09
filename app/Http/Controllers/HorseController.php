905978eed32a5a27165a8d8d05af52694a834110
keigo-toyama <104549980+keigo-tym@users.noreply.github.com>
2023-05-09 15:56:44 +0900

    HorseControllerの実装

--- app/Http/Controllers/HorseController.php
+++ app/Http/Controllers/HorseController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// ホースモデルを使用
+use App\Models\Horse;
 
 class HorseController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $items = Horse::all();
+        return view('horse.index', ['items' => $items]);
+    }
+
+    public function add(Request $request)
+    {
+        return view('horse.add');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Horse::$rules);
+        $horse = new Horse;
+        $form = $request->all();
+        unset($form['_token']);
+        $horse->fill($form)->save();
+        return redirect('/horse');
+    }
 }
