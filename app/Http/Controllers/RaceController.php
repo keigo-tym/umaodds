32c42c4b804727e81669503f57975a914149f57c
keigo-toyama <104549980+keigo-tym@users.noreply.github.com>
2023-05-09 12:01:17 +0900

    indexアクション

--- app/Http/Controllers/RaceController.php
+++ app/Http/Controllers/RaceController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// レースモデルを使用
+use App\Models\Race;
 
 class RaceController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $items = Race::all();
+        return view('race.index', ['items' => $items]);
+    }
 }
