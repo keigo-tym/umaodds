c1f50d7762508b43783bc1eba26898fc6887c6fe
keigo-toyama <104549980+keigo-tym@users.noreply.github.com>
2023-05-09 14:21:48 +0900

    race.addのビュー

---
+++ resources/views/race/add.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.umaodds')
+
+@section('title', 'Race.add')
+    
+@section('menubar')
+    @parent
+    レース作成ページ
+@endsection
+
+@section('content')
+    <form action="/race/add" method="post">
+    <table>
+        @csrf
+        <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+        <tr><th>date: </th><td><input type="text" name="date" value="{{old('date')}}"></td></tr>
+        <tr><th>place: </th><td><input type="text" name="place" value="{{old('place')}}"></td></tr>
+        <tr><th>surface: </th><td><input type="text" name="surface" value="{{old('surface')}}"></td></tr>
+        <tr><th>distance: </th><td><input type="number" name="distance" value="{{old('distance')}}"></td></tr>
+        <tr><th></th><td><input type="submit" value="send"></td></tr>
+    </table>
+    </form>
+@endsection
+
+@section('footer')
+copyright 2023 kei
+@endsection
