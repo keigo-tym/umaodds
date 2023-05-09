4331736ba22f07937b433876d0ba870dabba94bf
keigo-toyama <104549980+keigo-tym@users.noreply.github.com>
2023-05-09 15:51:20 +0900

    Horseモデルの作成

--- app/Models/Horse.php
+++ app/Models/Horse.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Horse extends Model
 {
     use HasFactory;
+
+    // idを自動で取得
+    protected $guarded = array('id');
+
+    // バリデージョンルールを設定
+    public static $rules = array(
+        'race_id' => 'required|integer',
+        'frame_number' => 'required|integer',
+        'horse_number' => 'required|integer',
+        'name' => 'required',
+        'advance_odds' => 'nullable|numeric',
+        'previous_odds' => 'nullable|numeric',
+        'twelve_odds' => 'nullable|numeric',
+        'fifteen_odds' => 'nullable|numeric',
+    );
 }
