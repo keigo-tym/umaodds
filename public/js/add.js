11c2220f6ec281f12d2d9a46ac2f037fdcff8584
keigo-toyama <104549980+keigo-tym@users.noreply.github.com>
2023-05-22 14:45:45 +0900

    add.jsにcolor選択要素を追加

--- public/js/add.js
+++ public/js/add.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
     addButton.on('click', function() {
         var newRow = $('<tr>');
         newRow.append('<input type="hidden" name="race_id" value="' + raceId + '">');
-        newRow.append('<td><input type="number" name="horses['+ counter +'][frame_number]" value=""></td>');
+        newRow.append('<td><input type="number" name="horses['+ counter +'][frame_number]" id="frame_number_'+ counter +'" value=""></td>');
         newRow.append('<td><input type="number" name="horses['+ counter +'][horse_number]" value=""></td>');
         newRow.append('<td><input type="text" name="horses['+ counter +'][name]" value=""></td>');
-        newRow.append('<td><select name="horses['+ counter +'][color]">' +
+        newRow.append('<td><select name="horses['+ counter +'][color]" id="color'+ counter +'">' +
             '<option value="">-----</option>' +
             '<option value="white">White</option>' +
             '<option value="black">Black</option>' +
@@ -23,6 +23,34 @@ $(document).ready(function() {
 
         formContainer.append(newRow);
 
+        var frameNumberInput = $('#frame_number_' + counter);
+        var colorSelect = $('#color' + counter);
+
+        frameNumberInput.on('input', function() {
+
+            var frameNumber = parseInt($(this).val());
+            var color = '';
+
+            if (frameNumber === 1) {
+                color = 'white';
+            } else if (frameNumber === 2) {
+                color = 'black';
+            } else if (frameNumber === 3) {
+                color = 'red';
+            } else if (frameNumber === 4) {
+                color = 'blue';
+            } else if (frameNumber === 5) {
+                color = 'yellow';
+            } else if (frameNumber === 6) {
+                color = 'green';
+            } else if (frameNumber === 7) {
+                color = 'orange';
+            } else if (frameNumber === 8) {
+                color = 'pink';
+            }
+
+            colorSelect.val(color);
+        });
         counter++;
     });
 });
